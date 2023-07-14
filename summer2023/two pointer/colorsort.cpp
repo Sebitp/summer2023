37d254890410cbfc1d32ff24648ea77f8efdbcf7
Your NameSebitP <nmbm119@gmail.com>
2023-07-14 02:03:28 -0400

    color sort pointers 7/13

---
+++ summer2023/two pointer/colorsort.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int r = nums.size()-1, mid = 0, l = 0;
+
+        while (mid<=r){
+            if(nums[mid] == 0){
+                swap(nums[mid],nums[l]);
+                l++;
+                mid++;
+            } else if (nums[mid] == 1){
+                mid++;
+            } else if (nums[mid] == 2){
+                swap(nums[mid],nums[r]);
+                r--;
+            }
+        }
+    }
+};
